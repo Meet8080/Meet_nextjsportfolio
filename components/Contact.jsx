16fab2684e9783b953baaf2d68743a49fd346a37
Meet8080 <patelarjun687@gmail.com>
2023-02-07 23:45:44 -0500

    fix linkedin icon link

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -41,7 +41,7 @@ const Contact = () => {
               </div>
               <div className="flex justify-between items-center max-w-[300px] m-auto py-6">
                 <div className="rounded-full shadow-md shadow-gray-600 p-3 cursor-pointer hover:scale-105 ease-in-out duration-500 bg-[#5655e5]">
-                <Link href="https://www.linkedin.com/in/meet-patel-299690213/"><FaLinkedinIn color="white" size={20}/></Link>
+                <FaLinkedinIn color="white" size={20}> <Link href="https://www.linkedin.com/in/meet-patel-299690213/"/></FaLinkedinIn>
                 </div>
                 <div className="rounded-full shadow-md shadow-gray-600 p-3 cursor-pointer hover:scale-105 ease-in-out duration-500 bg-[#5655e5]">
                 <Link href="https://github.com/Meet8080"> <FaGithub color="white" size={20} /></Link>
