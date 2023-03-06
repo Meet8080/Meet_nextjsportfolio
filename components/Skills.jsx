433d1584da2f5aa14a39c7d742d6fb6f533e0d66
Meet8080 <patelarjun687@gmail.com>
2023-03-06 17:53:21 -0500

    changed path for css logo

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 
 const Skills = () => {
   return (
-    <div id="skills" className="w-full lg:h-screen px-2 " >
+    <div id="skills" className="w-full lg:h-screen px-2 ">
       <div className="text-center py-4 md:py-12">
-        <h3 className="text-[#5655e5] text-3xl uppercase tracking-wider font-bold pt-16">Skills</h3>
+        <h3 className="text-[#5655e5] text-3xl uppercase tracking-wider font-bold pt-16">
+          Skills
+        </h3>
         {/* <hr className='w-[80%] mx-auto'></hr> */}
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 py-4">
@@ -24,7 +26,7 @@ const Skills = () => {
         </div>
         <div className="flex justify-around items-center shadow-lg shadow-gray-500">
           <Image
-            src={"assets/css2.png"}
+            src={"assets/css.png"}
             alt="tailwind-logo"
             width={70}
             height={70}
